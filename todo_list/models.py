149f1db305b8b86c5fc91fc3cf7cfa390c43104f
Vladyslav Bondarenko <128479458+VladyslavBon@users.noreply.github.com>
2024-11-27 15:23:56 +0200

    Add urls path

--- todo_list/models.py
+++ todo_list/models.py
@@ -9,11 +9,14 @@ class Tag(models.Model):
 
 
 class Task(models.Model):
-    content = models.TextField()
+    content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_completed", "-created_at"]
 
     def __str__(self):
         return self.content
